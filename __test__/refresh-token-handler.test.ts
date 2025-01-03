6aa13136b226604fdbb5e5fe747e206c8da57481
Dananjaya Ariyasena <74527180+ndkariyasena@users.noreply.github.com>
2025-01-03 21:45:35 +0100

    unit test update [skip ci]

--- __test__/refresh-token-handler.test.ts
+++ __test__/refresh-token-handler.test.ts
@@ -1,6 +1,8 @@
 import { TokenHandler } from '../src/module/refresh-token-handler';
 import { log } from '../src/lib/logger';
 import DefaultTokenStorage from '../src/module/token-storage';
+import { sign } from '../src/lib/signing-token';
+import { configure } from '../src/index';
 
 jest.mock('../src/module/token-storage');
 // jest.mock('../src/lib/logger', () => ({ default: { log: jest.fn() } }));
@@ -15,7 +17,7 @@ jest.mock('../src/lib/logger', () => ({
 }));
 
 describe('Refresh Token Handler', () => {
-	describe('Refresh token handling process related tests.', () => {
+	describe('Refresh token handler instance related tests.', () => {
 		it('01. Log method should be called with a warning message when the default token storage is used.', async () => {
 			/* const tokenGenerationHandler = {
         generateAuthToken: jest.fn(),
@@ -65,6 +67,42 @@ describe('Refresh Token Handler', () => {
 
 			expect(DefaultTokenStorage).toHaveBeenCalled();
 			// expect(tokenGenerationHandler).toHaveBeenCalled();
+		});
+	});
+
+	describe('"validateAuthToken" method related tests.', () => {
+		it('03. Should return the decoded token when the token is valid.', async () => {
+			const iss = 'https://example.com';
+			const aud = 'your-app';
+			const subject = 'test-subject';
+
+			const secret = 'SupperPass123';
+
+			configure({
+				publicKey: secret,
+			});
+
+			const token = (await sign({
+				payload: {},
+				secret,
+				options: {
+					issuer: iss,
+					audience: aud,
+					subject,
+				},
+			})) as unknown as string;
+
+			const tokenHandler = new TokenHandler({
+				// refreshTokenStorage: tokenStorage,
+				tokenGenerationHandler: jest.fn(),
+			});
+
+			const decoded = (await tokenHandler.validateAuthToken(token)) as unknown as Record<string, unknown>;
+
+			expect(decoded).not.toBeNull();
+			expect(decoded?.iss).toBe(iss);
+			expect(decoded?.aud).toBe(aud);
+			expect(decoded?.sub).toBe(subject);
 		});
 	});
 });
